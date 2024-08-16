c2af92d993bc05d1ffe8ccd1b2d0893fbc0a33a6
unknown <smd643925@gmail.com.>
2024-08-16 10:07:46 +0600

    header section done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,53 @@
+import { ImAngry2 } from "react-icons/im";
+
+const Header = () => {
+    return (
+    <div>
+      <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li><a>Home</a></li>
+        <li><a>Recipe</a></li>
+        <li><a>About</a></li>
+        <li><a>Search</a></li>
+      </ul>
+    </div>
+    <a className=" font-bold  text-xl">Recipe Calories</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Home</a></li>
+      <li><a>Recipe</a></li>
+      <li><a>About</a></li>
+      <li><a>Search</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end flex space-x-7">
+  <div className="form-control ">
+      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+    </div>
+    <button className="text-3xl text-green-400"><ImAngry2 /></button>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Header;
